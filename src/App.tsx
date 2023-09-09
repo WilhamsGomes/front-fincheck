66c03fef0e39ebe6ea2b9911eb3520628921adf8
Wilhams Gomes <71606861+WilhamsGomes@users.noreply.github.com>
2023-09-09 20:01:19 -0300

    feat: LaunchScreen and interception http client

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { Router } from "./Router";
 import { Toaster } from "react-hot-toast";
 import { AuthProvider } from "./app/contexts/AuthContext";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+	defaultOptions:{
+		queries:{
+			retry: false,
+			refetchOnWindowFocus: false,
+		}
+	}
+});
 
 export function App() {
 	return (
@@ -12,6 +20,7 @@ export function App() {
 				<Router />
 				<Toaster />
 			</AuthProvider>
+			<ReactQueryDevtools/>
 		</QueryClientProvider>
 	);
 }
--- src/app/contexts/AuthContext.tsx
+++ src/app/contexts/AuthContext.tsx
@@ -1,5 +1,9 @@
-import { createContext, useCallback, useState } from "react";
+import { createContext, useCallback, useEffect, useState } from "react";
 import { localStorageKeys } from "../config/localStorageKeys";
+import { useQuery } from "@tanstack/react-query";
+import { usersService } from "../services/usersService";
+import { toast } from "react-hot-toast";
+import { LaunchScreen } from "../../view/components/LaunchScreen";
 
 interface AuthContextValue {
 	signedIn: boolean;
@@ -11,10 +15,19 @@ export const AuthContext = createContext({} as AuthContextValue);
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
 	const [signedIn, setSignedIn] = useState<boolean>(() => {
-		const storedAccessToken = localStorage.getItem(localStorageKeys.ACCESS_TOKEN);
+		const storedAccessToken = localStorage.getItem(
+			localStorageKeys.ACCESS_TOKEN
+		);
 		return !!storedAccessToken;
 	});
-	
+
+	const { isError, isFetching, isSuccess, remove } = useQuery({
+		queryKey: ["loggedUser"],
+		queryFn: () => usersService.me(),
+		enabled: signedIn,
+		staleTime: Infinity,
+	});
+
 	const signin = useCallback((accessToken: string) => {
 		localStorage.setItem(localStorageKeys.ACCESS_TOKEN, accessToken);
 		setSignedIn(true);
@@ -22,12 +35,24 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
 
 	const signout = useCallback(() => {
 		localStorage.removeItem(localStorageKeys.ACCESS_TOKEN);
+		remove();
 		setSignedIn(false);
-	}, [])
+	}, [remove]);
+
+	useEffect(() => {
+		if (isError) {
+			toast.error("Sua sessão expirou");
+			signout();
+		}
+	}, [isError, signout]);
 
 	return (
-		<AuthContext.Provider value={{ signedIn, signin, signout }}>
-			{children}
+		<AuthContext.Provider
+			value={{ signedIn: isSuccess && signedIn, signin, signout }}
+		>
+			<LaunchScreen isLoading={isFetching}/>
+			
+			{!isFetching && children}
 		</AuthContext.Provider>
 	);
 }
